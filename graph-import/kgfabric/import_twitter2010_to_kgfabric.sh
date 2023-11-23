0c02a3fe4dc1858509476507a691fd29a83ff6cc
FessGo <yipeng4506336@sina.com>
2023-11-23 17:18:17 +0800

    Add files via upload

---
+++ graph-import/kgfabric/import_twitter2010_to_kgfabric.sh
@@ -0,0 +1,11 @@
+set -e
+KGFABRIC_BUILDER_PATH="kgfabric-local-builder.jar"
+DPATH="twitter2010"
+SCHEMA="twitter2010.schema"
+
+NAMESPACE="twitter2010"
+java -jar ${KGFABRIC_BUILDER_PATH} import
+--schema="${SCHEMA}" \
+--nodes=User="$DPATH/node.csv" \
+--relationships=CONNECT="$DPATH/../twitter-2010.csv" \
+"${NAMESPACE}"
