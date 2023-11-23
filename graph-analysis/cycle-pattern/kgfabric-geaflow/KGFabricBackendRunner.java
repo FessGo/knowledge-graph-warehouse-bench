182a7d5adfd69866bc080503a13973c62ec0f217
FessGo <yipeng4506336@sina.com>
2023-11-23 16:53:32 +0800

    Create  KGFabricBackendRunner.java

---
+++ graph-analysis/cycle-pattern/kgfabric-geaflow/KGFabricBackendRunner.java
@@ -0,0 +1,38 @@
+import com.antfin.aikg.kgfabric.adaptor.geaflow.benchmark.vc.3HopsCycleDetectionAlgorithm;
+import com.antfin.aikg.kgfabric.adaptor.geaflow.benchmark.Options;
+import com.antfin.aikg.kgfabric.adaptor.geaflow.structure.EdgeValueWrapper;
+import com.antfin.aikg.kgfabric.adaptor.geaflow.structure.GraphKey;
+import com.antfin.aikg.kgfabric.adaptor.geaflow.structure.VertexValueWrapper;
+import com.antfin.aikg.kgfabric.adaptor.geaflow.util.SourceUtil;
+import com.antfin.aikg.kgfabric.adaptor.geaflow.util.StateConfigBuilder;
+import com.antfin.arc.arch.api.env.Env;
+import com.antfin.arc.arch.api.job.mode.ISyncJob;
+import com.antfin.arc.cluster.context.ArchEnvContext;
+import com.antfin.arch.pdata.impl.graph.Graph;
+
+public class KGFabricBackendRunner {
+
+    public static int parallelism = 8;
+    public static void main(String[] args) {
+        ArchEnvContext context = (ArchEnvContext) Env.buildEnv();
+        context.withResource(parallelism);
+        context.setRunningMode(ArchEnvContext.RunningMode.default_mode);
+
+        StateConfigBuilder.newRocksDB()
+                .withFsUri(Options.FS_URI)
+                .withNamespace(Options.NAMESPACE)
+                .withVertexType(Options.VERTEX_TYPE)
+                .withEdgeType(Options.RELATION_TYPE)
+                .commit(context);
+
+        // no stage1#graph partition
+        context.submitJob((ISyncJob<Object, Long>) ((jobContext, input) -> {
+            Graph<GraphKey, VertexValueWrapper, EdgeValueWrapper> graph = Graph.buildGraph(jobContext, SourceUtil.getNoShuffleSource(parallelism));
+            // stage2#iterations
+            return graph.runVCIterator(new 3HopCycleDetectionAlgorithm(), parallelism)
+                    .getVertexDataSet()
+                    .get();
+        }));
+
+    }
+}
